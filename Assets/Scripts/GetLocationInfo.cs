6a4cfea72c99e6e8826d1f3d6f701e93eee4ae3e
jimmyCode <yellowcrayon@126.com>
2017-11-18 12:35:32 +0800

    Project Init
    Project InitProject InitProject InitProject Init

---
+++ Assets/Scripts/GetLocationInfo.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GetLocationInfo : MonoBehaviour {
+
+    public Text text;
+    // Use this for initialization
+    void Start()
+    {
+        InvokeRepeating("StartLocation", 1, 5);
+    }
+
+    public void StartLocation()
+    {
+        AndroidJavaClass jc = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        AndroidJavaObject jo = jc.GetStatic<AndroidJavaObject>("currentActivity");
+        text.text = jo.Call<string>("GetInfo");
+    }
+}
